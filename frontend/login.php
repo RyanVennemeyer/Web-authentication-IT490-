2a60680e08fc16077fccd7e1631efe246f462375
Ryan Vennemeyer <51283756+RyanVennemeyer@users.noreply.github.com>
2022-10-30 18:14:22 -0400

    Add files via upload

--- frontend/login.php
+++ frontend/login.php
@@ -6,10 +6,25 @@
     require_once('../rabbitmqphp_example/rabbitMQLib.inc');
     require_once('rabbitMQClient.php');
 
-    $client = new rabbitMQClient("../config/rabbitConf.ini","testServer");
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
- 
-    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $_SESSION["username"]=$_POST['username'];
+    $request = array();
+    $request['type'] = "Login";
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $request['message'] = $msg;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+
+    echo "client received response: ".PHP_EOL;
+    print_r($response);
+    echo "\n\n";
+
+    echo $argv[0]." END".PHP_EOL;
+    /*
     function login($username, $password){
         
         $request = array();
@@ -25,4 +40,6 @@
 
         echo $response["returnCode"];
         return $request;
-    }
+    }*/
+
+?>
