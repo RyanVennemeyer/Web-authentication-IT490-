bee0897e753e5b94b615c71597f68f36d5f75e38
Jcontreras911 <72625958+Jcontreras911@users.noreply.github.com>
2022-11-02 11:59:50 -0400

    Add files via upload

--- frontend/login.php
+++ frontend/login.php
@@ -1,29 +1,52 @@
+
 <?php
-    session_start();
-    //  Requireing required files
-    require_once('../rabbitmqphp_example/path.inc');
-    require_once('../rabbitmqphp_example/get_host_info.inc');
-    require_once('../rabbitmqphp_example/rabbitMQLib.inc');
-    require_once('rabbitMQClient.php');
 
-    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+session_start();
+//  Require-ing required files
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $_SESSION["username"]=$_POST['username'];
-    $request = array();
-    $request['type'] = "Login";
-    $request['username'] = $username;
-    $request['password'] = $password;
-    $request['message'] = $msg;
-    $response = $client->send_request($request);
-    //$response = $client->publish($request);
+require_once('path.inc');
 
-    echo "client received response: ".PHP_EOL;
-    print_r($response);
-    echo "\n\n";
+require_once('get_host_info.inc');
 
-    echo $argv[0]." END".PHP_EOL;
-  
+require_once('rabbitMQLib.inc');
+
+//require_once('rabbitMQClient.php');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+if (!isset($_POST))
+{
+    $msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+    echo json_encode($msg);
+    exit(0);
+}
+
+$request = $_POST;
+$response = "unsupported request type, politely FUCK OFF";
+$username = $_POST["username"];
+$password = $_POST["password"];
+$_SESSION["username"]=$_POST['username'];
+
+$arr = array();
+$arr['type'] = "login";
+$arr['username'] = $username;
+$arr['password'] = $password;
+//$name = "$username $password";
+
+$send = $client->send_request($arr);
+
+
+switch ($request["type"])
+{
+    case "login":
+        $response = "login, yeah we can do that";
+    break;
+}
+
+//echo json_encode($request);
+//echo "". PHP_EOL;
+//echo json_encode($_POST);
+var_dump($arr);
+exit(0);
 
 ?>
