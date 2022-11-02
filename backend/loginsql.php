af6deb7d50dbca0065870f482e85255b53f3f638
Ryan Vennemeyer <51283756+RyanVennemeyer@users.noreply.github.com>
2022-11-02 00:57:37 -0400

    Add files via upload

--- backend/loginsql.php
+++ backend/loginsql.php
@@ -1,52 +1,89 @@
 
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-$mydb = new mysqli('127.0.0.1','testUser','12345','testdb');
-$utest = $_POST["username"];
-$ptest = $_POST["password"];
-echo "$utest $ptest";
-$username="guest";
-$password="password123";
-if ($mydb->errno != 0)
-{
-	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
-	exit(0);
-}
 
 echo "successfully connected to database".PHP_EOL;
 
-//$query = "select * from testtable;";
-$querynametest = "select * from user where username='$username' and password='$password';";
+function login($username,$password){
+	$mydb = new mysqli('127.0.0.1','testUser','12345','testdb');
+	$utest = $_POST["username"];
+	$ptest = $_POST["password"];
+	echo "$utest $ptest";
+	if ($mydb->errno != 0)
+	{
+		echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+		exit(0);
+	}
+	$username="guest";
+	$password="password123";
 
-//$response = $mydb->query($query);
 
-$response2= $mydb->query($querynametest);
-if ($mydb->errno != 0)
-{
-	echo "failed to execute query:".PHP_EOL;
-	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
-	exit(0);
+	//$query = "select * from testtable;";
+	$querynametest = "select * from user where username='$username' and password='$password';";
+
+	//$response = $mydb->query($query);
+
+	$response2= $mydb->query($querynametest);
+	if ($mydb->errno != 0)
+	{
+		echo "failed to execute query:".PHP_EOL;
+		echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+		exit(0);
+	}
+	//$numrows = mysqli_num_rows($response);
+	$users = mysqli_num_rows($response2);
+	//echo "we got $numrows from the query".PHP_EOL;
+
+	/*while ($row = $response->fetch_row())
+	{
+		print_r($row);
+	}
+	*/
+
+	echo "test complete".PHP_EOL;
+	if($users==1)
+	{
+	echo "pass".PHP_EOL;
+	}
+	else
+	{
+	echo "fail".PHP_EOL;
+	}
+	echo "plz work $users".PHP_EOL;
+	return true;
 }
 
-//$numrows = mysqli_num_rows($response);
-$users = mysqli_num_rows($response2);
-//echo "we got $numrows from the query".PHP_EOL;
 
-/*while ($row = $response->fetch_row())
-{
-	print_r($row);
-}
- */
 
-echo "test complete".PHP_EOL;
-if($users==1)
-{
-echo "pass".PHP_EOL;
-}
-else
-{
-echo "fail".PHP_EOL;
-}
-echo "plz work $users".PHP_EOL;
 
+
+
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    //case "validate_session":
+      //return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
 ?>
